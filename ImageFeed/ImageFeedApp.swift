c2bfbaed0cbae33e3bf6fe5fca7ef3983e36c829
Kimjunhwan <tazanboy1@inavi.kr>
2026-02-18 15:03:54 +0900

    Initial Commit

---
+++ ImageFeed/ImageFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedApp.swift
+//  ImageFeed
+//
+//  Created by JunHwan Kims on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct ImageFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
